b57773fafef403a7650bb0f645d51d9c03e62a11
Alonso Allen Chaves Santa Helena <alonsodemais@gmail.com>
2022-07-17 00:33:15 -0300

    Atualização para a Prévia
    Telas ajustadas para reproduzir o fluxo de compra.  Adição de tela para pagamento via pix e outros ajustes menores também foram feitos.

--- Presentation/template/mail/contact.php
+++ Presentation/template/mail/contact.php
@@ -9,12 +9,12 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "seuemail@mail.com"; // para funcionar, necessário ligar um host de email na máquina // 
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $subject, $body, $header)) 
   http_response_code(500);
-?>
+?> 
